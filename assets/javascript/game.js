0931cbc3f9d1b543acb29162e5731890c803f7d4
Michael Williams <mwilliams2964@gmail.com>
2019-09-09 20:14:09 -0700

    added hover css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,38 @@
 $(document).ready(function () {
+    let fighters = [
+
+        {
+            name: "obi",
+            healthPoints: 120,
+            attackPower: 8,
+            counterAttackPower: 16
+        },
+        {
+            name: "maul",
+            healthPoints: 180,
+            attackPower: 7,
+            counterAttackPower: 14
+        },
+        {
+            name: "skywalker",
+            healthPoints: 100,
+            attackPower: 6,
+            counterAttackPower: 12,
+        },
+        {
+            name: "sidious",
+            healthPoints: 150,
+            attackPower: 9,
+            counterAttackPower: 18
+        }
+    ]
+    
     let characters = ["obi", "maul", "skywalker", "sidious"];
 
     // This dynamically creates boxes with each assigned to a character
     for (let i = 0; i < characters.length; i++) {
         let character = $("<div>");
-        character.addClass("character");
+        character.addClass("player character");
         character.attr("data-name", characters[i]);
         character.text(characters[i]);
         $("#players").append(character);
@@ -12,12 +40,12 @@ $(document).ready(function () {
 
     // This lets you choose a player, and puts the rest of the players
     // into the enemy section
-    $("#players").on("click", ".character", function () {
-        
-        $(".character").addClass("enemy");
+    $("#players").on("click", ".player", function () {
+
+        $(".player").addClass("enemy");
         $("#attackers").append($(".enemy"));
         $("#players").append($(this));
-        $(this).attr("class", "character");
+        $(this).toggleClass("enemy player");
     })
 
     // This lets you choose an enemy to fight and puts your opponent 
